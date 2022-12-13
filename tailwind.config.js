5525d7577cb87c7e835745c205cfbf4b952ef0b4
drajatdani1892 <drajat053@ummi.ac.id>
2022-12-13 14:41:19 +0700

    Feat: Config TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#00D87F",
+                secondary: "#00A162",
+                accent: "#001F26",
+                accent2: "#F3F4F5",
+                gray: "#ACC6C6",
+            },
+            screens: {
+                laptopLg: "1160px",
+                laptopXl: "1360px",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
